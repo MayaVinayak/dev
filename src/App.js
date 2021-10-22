ec43b41dc841459a0bf1d522c7728844644c73b5
Maya Vinayak <mayavinayakskp@gmail.com>
2021-10-22 12:04:49 +0530

    all

--- src/App.js
+++ src/App.js
@@ -1,17 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
-import Greet from "./components/Greet"
-import Welcome from "./components/Welcome"
-import Message from './components/message';
-import Counter from './components/Counter';
-import Parent from './components/Parent';
-import Form from './components/Form';
+import Frag from './components/frag';
 
 
 function App() {
   return (
     <div className="App">
-      <Form />
+      <Frag />
     </div>
   );
 }
